91888e60ba238ae500a067ea94b1740c81565ffa
Felix Horger <felix.horger@kcl.ac.uk>
2025-03-30 10:30:00 +0100

    Merged pdftools.py and utils.py

--- utils.py
+++ utils.py
@@ -2,7 +2,22 @@ import os
 import subprocess
 import mimetypes
 import html2text as HTML2Text
+import fitz
 
+
+
+def pdf2text(path):
+	with fitz.open(path) as doc:
+		text = ""
+		for page in doc: text += page.get_text() + " "
+		text = text[:-1] 
+		title = doc.metadata["title"]
+	return title, text
+
+# Maybe useful
+#def sanitize(filename):
+#    valid_chars = "-_.() %s%s" % (string.ascii_letters, string.digits)
+#    return "".join([c for c in filename if c in valid_chars])
 
 
 # TODO: make this proper with config file
